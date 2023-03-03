f83c0b043258dc07a0147c4877318f835dfb7133
Michael Liu <michael.liu.mike@gmail.com>
2023-03-03 12:08:19 -0800

    attempting to fix routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import Header from './Header'
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App(){
-  return(
+function App() {
+  return (
     <div className="App">
-
-      {/* Header */}
       <Header />
-      {/* study match */}
-      {/* buttons below cards */}
-
-      {/* matches inbox */}
-      {/* individual matches */}
+      <Router>
+        <Routes>
+          <Route path="/chat" exact>
+            <h1> hello chat</h1>
+          </Route>
+          <Route path="/" exact>
+            <h1> I am home page</h1>
+          </Route>
+        </Routes>
+      </Router>   
     </div>
-
-
-
   )
 }
 
