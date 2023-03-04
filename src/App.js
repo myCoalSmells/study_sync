2d3d4e90de9119ca52526d3511156ceb71051e2e
Michael Liu <michael.liu.mike@gmail.com>
2023-03-03 18:58:32 -0800

    added swiepable cards, data from firebase to be added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Header from './Header'
+import Header from './Header';
 import "./App.css";
+import ProfileCards from "./ProfileCards";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/chat" element={<h1>i am chat page</h1>}/>
-          <Route path="/" element={<h1>i am home page</h1>}/>
+          <Route path="/" element={<h1><ProfileCards /></h1>}/>
         </Routes>
       </Router>   
     </div>
---
+++ src/ProfileCards.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import TinderCard from 'react-tinder-card';
+import "./ProfileCards.css";
+
+function ProfileCards() {
+  const [students, setStudents] = useState([
+    {
+      name: 'Michael Liu',
+      pfp: "https://i.imgur.com/pkpvLJn.jpeg",
+      classMatch: "",
+      availTime: "",
+      contactInfo: ""  
+    },
+    {
+      name: 'Harry Hinman',
+      pfp: "https://i.imgur.com/ZAcA6w9.jpeg",
+      classMatch: "",
+      availTime: "",
+      contactInfo: ""
+    }
+  ]);
+
+  return (
+    <div>
+      <h1>Student Cards</h1>
+      <div className="studentCards__container">
+        {students.map(student => (
+            <TinderCard
+            className="swipe"
+            key={student.name}
+            preventSwipe={['up', 'down']}
+            >
+            <div style={{ backgroundImage: `url(${student.pfp})` }} className="card">
+                <h1>{student.name}</h1>
+            </div>
+            </TinderCard>
+        ))}
+        </div>
+
+    </div>
+
+  );
+}
+
+export default ProfileCards;
