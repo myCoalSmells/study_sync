e3cce4f35cc17864f1a70f821d69ec86b1eebc46
Joshua Kim <jdkim5136@gmail.com>
2023-03-04 16:55:11 -0800

    Added Temp UI for calendar schedule

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,27 +1,18 @@
-import React, { useEffect, useState } from "react";
-
+//import React, { useEffect, useState } from "react";
+import React from "react";
 import PPMod from "./ProfilePage.module.css";
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function ProfilePage() {
-    const [students, setStudents] = useState([
-    {
-        name: 'Michael Liu',
-        pfp: "https://i.imgur.com/pkpvLJn.jpeg",
-        classMatch: "",
-        availTime: "",
-        contactInfo: ""  
-    },
-    {
-        name: 'Harry Hinman',
-        pfp: "https://i.imgur.com/ZAcA6w9.jpeg",
-        classMatch: "",
-        availTime: "",
-        contactInfo: ""
-    }
-    ]);
+export default function ProfilePage() {
+
+    /* 
+        when edit student profile button gets clicked
+        make sure to send the student's data through to 
+        the EditProfilePage.
+    */
+    let availableTimes = [4][7];
 
     return(
         <div className={PPMod.container}>
@@ -32,18 +23,52 @@ function ProfilePage() {
                 <h1 className={PPMod.title}>
                     StudentName's Profile
                 </h1>
-                <Button className={PPMod.button}>
+                <Button variant="outline-primary">
                     Edit Profile
                 </Button>
             </div>
             <p>
-                Classes: __ , __ , __ , ...
+                Classes: class1 , class2 , class3 , ...
             </p>
             <p>
-                Times: Importing a module for this
+                Schedule: 
             </p>
+            <div>
+                <Button variant="outline-secondary">Sun</Button>
+                <Button variant="outline-secondary">Mon</Button>
+                <Button variant="outline-secondary">Tue</Button>
+                <Button variant="outline-secondary">Wed</Button>
+                <Button variant="outline-secondary">Thu</Button>
+                <Button variant="outline-secondary">Fri</Button>
+                <Button variant="outline-secondary">Sat</Button>
+            </div>
+            <div>
+                <Button variant="outline-secondary">Sun</Button>
+                <Button variant="outline-secondary">Mon</Button>
+                <Button variant="outline-secondary">Tue</Button>
+                <Button variant="outline-secondary">Wed</Button>
+                <Button variant="outline-secondary">Thu</Button>
+                <Button variant="outline-secondary">Fri</Button>
+                <Button variant="outline-secondary">Sat</Button>
+            </div>
+            <div>
+                <Button variant="outline-secondary">Sun</Button>
+                <Button variant="outline-secondary">Mon</Button>
+                <Button variant="outline-secondary">Tue</Button>
+                <Button variant="outline-secondary">Wed</Button>
+                <Button variant="outline-secondary">Thu</Button>
+                <Button variant="outline-secondary">Fri</Button>
+                <Button variant="outline-secondary">Sat</Button>
+            </div>
+            <div>
+                <Button variant="outline-secondary">Sun</Button>
+                <Button variant="outline-secondary">Mon</Button>
+                <Button variant="outline-secondary">Tue</Button>
+                <Button variant="outline-secondary">Wed</Button>
+                <Button variant="outline-secondary">Thu</Button>
+                <Button variant="outline-secondary">Fri</Button>
+                <Button variant="outline-secondary">Sat</Button>
+            </div>
         </div>
     );
 }
-
-export default ProfilePage;
