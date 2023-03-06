9be1967e93305dde113f0ab9e4ad07eabf58b55c
Harry Hinman <harryhinman257@gmail.com>
2023-03-05 19:14:15 -0800

    added mock sign up
    made mock sign up which added name and contact info into the database, navigate to /fbtest and enter info and submit to see updates in firebase

--- src/FirebaseTest.js
+++ src/FirebaseTest.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { app, db } from "./firebase_setup/firebase";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
+
 import "./FirebaseTest.css";
 
 function FirebaseTest () {
-    return (
-        <div>FirebaseTest</div>
+  const [data, setData] = useState({});
+  const collectionRef = collection(db, "students");
+
+  const handleInput = (event) => {
+    let newInput = { [event.target.name]: event.target.value };
+
+    setData({ ...data, ...newInput })
+  }
+
+  const handleSubmit = () => {
+    addDoc(collectionRef, {
+      name: data.name,
+      contactInfo: data.contactInfo
+    })
+  }
+
+  return (
+      <div>
+        <input
+          placeholder="Name"
+          name="name"
+          type="name"
+          onChange={event => handleInput(event)}
+        />
+        <input
+          placeholder="Contact Info"
+          name="contactInfo"
+          type="contactInfo"
+          onChange={event => handleInput(event)}
+        />
+
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
     );
 }
 
