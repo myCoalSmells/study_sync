5443bbabb4160c339bf479521a0d7d2722bb068e
fatehss <fatehss@g.ucla.edu>
2023-03-07 11:56:23 -0800

    Update addStudent function for user creation
    In this commit I used the admin.auth() library to call upon admin.auth.createUser() to create a user in the database. This is different from the previous version as the last version of the function did not create a user in firebase and make use of its authentication tools, it simply added the user's credentials to the firestore database.

--- functions/index.js
+++ functions/index.js
@@ -3,50 +3,30 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('messages').add({original: original});
-  // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
-});
-
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-    .onCreate((snap, context) => {
-      // Grab the current value of what was written to Firestore.
-      const original = snap.data().original;
-
-      // Access the parameter `{documentId}` with `context.params`
-      functions.logger.log('Uppercasing', context.params.documentId, original);
-      
-      const uppercase = original.toUpperCase();
-      
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to Firestore.
-      // Setting an 'uppercase' field in Firestore document returns a Promise.
-      return snap.ref.set({uppercase}, {merge: true});
-    });
 
 
-    // Take the text parameter passed to this HTTP endpoint and insert it into 
-// Firestore under the path /messages/:documentId/original
+/**
+ * This function creates a user as well as adds the user's information to the firestore database
+ */
+//The url for this is: http://127.0.0.1:5001/study-sync-23/us-central1/addStudent?firstname=John&lastname=Smith&schedule=1010101001000100101001010010101001&emailaddress=who@what.com&password=asdfasdf
 exports.addStudent = functions.https.onRequest(async (req, res) => {
     // Grab the text parameter.
     
     try { 
         const firstname = req.query.firstname;
         const lastname = req.query.lastname;
+        const schedule = req.query.schedule
         const email = req.query.emailaddress;
+        const password = req.query.password;
 
+        const userRecord = await admin.auth().createUser({
+          email: email,
+          password: password
+        });
         const writeResult = await admin.firestore().collection('students').add({firstName: firstname,
-        lastName: lastname, emailAddress: email});
+        lastName: lastname, schedule:schedule});
 
-        res.json({result: `Message with ID: ${writeResult.id} added.`});
+        res.json({result: `Student with ID: ${writeResult.id} added.`});
     } catch (error) {
     // Handle errors
     console.error(error);
@@ -54,3 +34,5 @@ exports.addStudent = functions.https.onRequest(async (req, res) => {
     }
 
   });
+
+
