b05b804b31321cf6d7deadb042399143002daa82
Harry Hinman <harryhinman257@gmail.com>
2023-03-11 13:50:08 -0800

    signup page now adds user info to both firestore and authenticator

--- src/SignupPage.js
+++ src/SignupPage.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
 import LPMod from "./LoginPage.module.css";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./firebase-setup/firebase"
+import { collection, setDoc, doc } from "firebase/firestore";
+import { auth, firestore } from "./firebase-setup/firebase"
 
 export default function SignupPage() {
-
+    const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [pic, setPic] = useState("");
+    const colRef = collection(firestore, "students");
 
     const signUp = (e) => {
         e.preventDefault();
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            console.log(userCredential);
+            const user = auth.currentUser;
+            setDoc(doc(firestore, "students", user.uid), {
+                username: username,
+                email: email,
+                password: password,
+                pfp: pic,
+                key: user.uid
+            })
+            console.log(user.uid);
         })
         .catch((error) => {
             console.log(error);
@@ -25,6 +36,14 @@ export default function SignupPage() {
         <form onSubmit={signUp}>
             <label>
                 <p>Username</p>
+                <input type="text" 
+                onChange={(e) => setUsername(e.target.value)}
+                />
+            </label>
+            <div>
+            </div>
+            <label>
+                <p>Email</p>
                 <input type="text"
                 onChange={(e) => setEmail(e.target.value)}
                  />
@@ -41,7 +60,9 @@ export default function SignupPage() {
             </div>
             <label>
                 <p>Profile Picture (URL)</p>
-                <input type="url" />
+                <input type="url"
+                onChange={(e) => setPic(e.target.value)}
+                />
             </label>
             <div className={LPMod.container}>
                 <button type="submit">Sign Up</button>
