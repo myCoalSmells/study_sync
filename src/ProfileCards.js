8aa8bbc2f947c93ac7d4fdda61ab8e8afbc28231
Michael Liu <michael.liu.mike@gmail.com>
2023-03-11 09:46:42 -0800

    main page firebase maint

--- src/ProfileCards.js
+++ src/ProfileCards.js
@@ -1,60 +1,71 @@
-//import React, { useEffect, useState } from "react";
-import React, { useState } from "react";
+import { collection, doc, query, onSnapshot } from "firebase/firestore";
+import database from "./firebase";
+import React, { useEffect, useState } from "react";
 import TinderCard from 'react-tinder-card';
 import PCMod from "./ProfileCards.module.css";
 
 function ProfileCards() {
-  const [students, setStudents] = useState([
-    {
-      name: 'Michael Liu',
-      pfp: "https://i.imgur.com/pkpvLJn.jpeg",
-      classMatch: "",
-      availTime: "",
-      contactInfo: ""  
-    },
-    {
-      name: 'Bob Lee',
-      pfp: "https://i.imgur.com/ZAcA6w9.jpeg",
-      classMatch: "",
-      availTime: "",
-      contactInfo: ""
-    },
-    {
-      name: 'Michael Jordan',
-      pfp: "https://i.imgur.com/ZAcA6w9.jpeg",
-      classMatch: "",
-      availTime: "",
-      contactInfo: ""
-    },
-    {
-      name: 'Barack Obama',
-      pfp: "https://i.imgur.com/ZAcA6w9.jpeg",
-      classMatch: "",
-      availTime: "",
-      contactInfo: ""
-    },
+  const [students, setStudents] = useState([]);
 
-  ]);
+  useEffect(() => { 
+    const q = query(collection(database, "students"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      setStudents(querySnapshot.docs.map(doc => doc.data()));
+    });
+
+    // Unsubscribe from listener when component unmounts
+    return unsub;
+  }, []);
+
+  // const [students, setStudents] = useState([
+  //   {
+  //     name: 'Michael Liu',
+  //     pfp: "https://i.imgur.com/pkpvLJn.jpeg",
+  //     classMatch: "",
+  //     availTime: "",
+  //     contactInfo: ""  
+  //   },
+  //   {
+  //     name: 'Bob Lee',
+  //     pfp: "https://i.imgur.com/ZAcA6w9.jpeg",
+  //     classMatch: "",
+  //     availTime: "",
+  //     contactInfo: ""
+  //   },
+  //   {
+  //     name: 'Michael Jordan',
+  //     pfp: "https://i.imgur.com/ZAcA6w9.jpeg",
+  //     classMatch: "",
+  //     availTime: "",
+  //     contactInfo: ""
+  //   },
+  //   {
+  //     name: 'Barack Obama',
+  //     pfp: "https://i.imgur.com/ZAcA6w9.jpeg",
+  //     classMatch: "",
+  //     availTime: "",
+  //     contactInfo: ""
+  //   },
+
+  // ]);
 
   return (
     <div>
       <h1>Student Cards</h1>
       <div className={PCMod.studentCards__container}>
         {students.map(student => (
-            <TinderCard
-              className={PCMod.swipe}
-              key={student.name}
-              preventSwipe={['up', 'down']}
-            >
-              <div style={{ backgroundImage: `url(${student.pfp})` }} className={PCMod.card}>
-                  <h1>{student.name}</h1>
-              </div>
-            </TinderCard>
+          <TinderCard
+            className={PCMod.swipe}
+            key={student.name}
+            preventSwipe={['up', 'down']}
+          >
+            <div style={{ backgroundImage: `url(${student.pfp})` }} className={PCMod.card}>
+              <h1>{student.name}</h1>
+            </div>
+          </TinderCard>
         ))}
-        </div>
-
+      </div>
     </div>
-
   );
 }
 
