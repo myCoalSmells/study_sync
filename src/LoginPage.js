2f77558de626f9a04bb49cf43a5e0f037dffacf8
Tiffany Tsou <ttsou1234@ucla.edu>
2023-03-11 11:23:21 -0800

    changed format of login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import LPMod from "./LoginPage.module.css";
 
 export default function LoginPage() {
@@ -10,13 +11,20 @@ export default function LoginPage() {
                 <p>Username</p>
                 <input type="text" />
             </label>
+            <div>
+            </div>
             <label>
                 <p>Password</p>
                 <input type="password" />
             </label>
-            <h1>Don't have an account?</h1>
-            <div>
+            <div className={LPMod.container}>
+                <button type="submit">Login</button>
+            </div>
+            <div className={LPMod.login}>
+                <h1>Don't have an account?</h1>
+                <Link to="/signup">
                 <button type="submit">Sign Up</button>
+                </Link>
             </div>
             </form>
         </div>
