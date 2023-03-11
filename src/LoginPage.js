e65cf65aafc3564d350f73bddf820c80116e5708
fateh <fatehss@g.ucla.edu>
2023-03-11 13:52:26 -0800

    add login functionality

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,31 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import LPMod from "./LoginPage.module.css";
 import handleSubmit from "./firebase-setup/handlesubmit";
 import { useRef } from "react";
+import { auth } from "./firebase-setup/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function LoginPage() {
-    const dataRef = useRef()
 
-    const submithandler = (e) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = (e) => {
+
         e.preventDefault()
-        handleSubmit(dataRef.current.value)
-        dataRef.current.value = ""
-        console.log('submitted')
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) =>{
+            console.log(userCredential);
+            console.log("sign in!");
+        })
+        .catch((error) => {
+            console.log(error);
+            console.log("no sign in");
+        })
     }
     return (
         <div className={LPMod.login}>
         <h1>Login to StudySync</h1>
-        <form onSubmit={submithandler}>
+        <form onSubmit={signIn}>
             <label>
                 <p>Username</p>
-                <input type="text" ref={dataRef} />
+                <input type="text" 
+                onChange={(e) => setEmail(e.target.value)}/>
             </label>
             <div>
             </div>
             <label>
                 <p>Password</p>
-                <input type="password" />
+                <input type="password" 
+                onChange={(e) => setPassword(e.target.value)}/>
             </label>
             <div className={LPMod.container}>
                 <button type="submit">Login</button>
