41c423534c861d38bd24aebbccbaa6b4e5ebffa7
Tiffany Tsou <ttsou1234@ucla.edu>
2023-03-11 14:12:58 -0800

    added links to homepage/login on logout page

--- src/LogoutPage.js
+++ src/LogoutPage.js
@@ -1,15 +1,20 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import LPMod from "./LoginPage.module.css";
 
 export default function LogoutPage() {
     return (
         <div className={LPMod.login}>
         <h1>You have been logged out.</h1>
-        <div>
+        <div className={LPMod.container}>
+        <Link to="/login">
             <button type="submit">Login</button>
+        </Link>
         </div>
         <div>
+        <Link to="/">
             <button type="submit">Return to Homepage</button>
+        </Link>
         </div>
         </div>
     )
