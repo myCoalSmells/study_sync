b62c122289f47559e00ab227a3e61bd206a0c4a9
Harry Hinman <harryhinman257@gmail.com>
2023-03-11 16:36:01 -0800

    allowed for dynamic data to be displayed on the profile page based on whose logged in

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,5 +1,5 @@
 //import React, { useEffect, useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 import PPMod from "./ProfilePage.module.css";
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,24 +14,37 @@ export default function ProfilePage() {
         make sure to send the student's data through to 
         the EditProfilePage.
     */
-    
-    
     const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
+    const [username, setUsername] = useState("");
+    const [classMatch, setClassMatch] = useState("");
+    const [contactInfo, setContactInfo] = useState("");
+    const [availTime, setAvailTime] = useState("");
+    const [login, setLogin] = useState(false);
+    onAuthStateChanged(auth, async (user) => {
         if (user) {
             const docRef = doc(firestore, "students", user.uid);
-            const docSnap = getDoc(docRef);
-            console.log(docRef.id);
-            console.log("logged in");
-        } else {
-            console.log("not logged in");
-        }
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                const data = docSnap.data();
+                setUsername(docSnap.get("username"));
+                setClassMatch(docSnap.get("classMatch"));
+                setContactInfo(docSnap.get("email"));
+                setAvailTime(docSnap.get("availTime"));
+                setLogin(true);
+            } else {
+            console.log("Document does not exist");
+            }
+        console.log(docRef.id);
+
+        
+    } else {
+        console.log("not logged in");
+    }
     });
 
-
+    /*
    const DAYS = 7;
    const WEEKS = 4;
-
     // Temp Student Data Format
     // Extract firebase and put into this format?
     let Student = {
@@ -41,66 +54,74 @@ export default function ProfilePage() {
         availTime: "0101001001110000101000101010",
         contactInfo: "myemail@yahoo.com"  
     };
+    */
 
-
-    return(
-        <div className={PPMod.container}>
-            <div className={PPMod.subcontainer}>
-                <div>
-                    
+    if (login === true) {
+        return(
+            <div className={PPMod.container}>
+                <div className={PPMod.subcontainer}>
+                    <div>
+                        
+                    </div>
+                    <h1 className={PPMod.title}>
+                        {username}'s Profile
+                    </h1>
+                    <Button variant="outline-primary">
+                        Edit Profile
+                    </Button>
                 </div>
-                <h1 className={PPMod.title}>
-                    {Student.name}'s Profile
-                </h1>
-                <Button variant="outline-primary">
-                    Edit Profile
-                </Button>
+                <p>
+                    Classes: {classMatch}
+                </p>
+                <p>
+                    Contact: {contactInfo}
+                </p>
+                <p>
+                    Schedule: 
+                </p>
+                <div>
+                    <Button variant="outline-secondary">Sun {availTime[0]}</Button>
+                    <Button variant="outline-secondary">Mon {availTime[1]}</Button>
+                    <Button variant="outline-secondary">Tue {availTime[2]}</Button>
+                    <Button variant="outline-secondary">Wed {availTime[3]}</Button>
+                    <Button variant="outline-secondary">Thu {availTime[4]}</Button>
+                    <Button variant="outline-secondary">Fri {availTime[5]}</Button>
+                    <Button variant="outline-secondary">Sat {availTime[6]}</Button>
+                </div>
+                <div>
+                    <Button variant="outline-secondary">Sun {availTime[7]}</Button>
+                    <Button variant="outline-secondary">Mon {availTime[8]}</Button>
+                    <Button variant="outline-secondary">Tue {availTime[9]}</Button>
+                    <Button variant="outline-secondary">Wed {availTime[10]}</Button>
+                    <Button variant="outline-secondary">Thu {availTime[11]}</Button>
+                    <Button variant="outline-secondary">Fri {availTime[12]}</Button>
+                    <Button variant="outline-secondary">Sat {availTime[13]}</Button>
+                </div>
+                <div>
+                    <Button variant="outline-secondary">Sun {availTime[14]}</Button>
+                    <Button variant="outline-secondary">Mon {availTime[15]}</Button>
+                    <Button variant="outline-secondary">Tue {availTime[16]}</Button>
+                    <Button variant="outline-secondary">Wed {availTime[17]}</Button>
+                    <Button variant="outline-secondary">Thu {availTime[18]}</Button>
+                    <Button variant="outline-secondary">Fri {availTime[19]}</Button>
+                    <Button variant="outline-secondary">Sat {availTime[20]}</Button>
+                </div>
+                <div>
+                    <Button variant="outline-secondary">Sun {availTime[21]}</Button>
+                    <Button variant="outline-secondary">Mon {availTime[22]}</Button>
+                    <Button variant="outline-secondary">Tue {availTime[23]}</Button>
+                    <Button variant="outline-secondary">Wed {availTime[24]}</Button>
+                    <Button variant="outline-secondary">Thu {availTime[25]}</Button>
+                    <Button variant="outline-secondary">Fri {availTime[26]}</Button>
+                    <Button variant="outline-secondary">Sat {availTime[27]}</Button>
+                </div>
             </div>
-            <p>
-                Classes: {Student.classMatch}
-            </p>
-            <p>
-                Contact: {Student.contactInfo}
-            </p>
-            <p>
-                Schedule: 
-            </p>
+        );
+    } else {
+        return (
             <div>
-                <Button variant="outline-secondary">Sun {Student.availTime[0]}</Button>
-                <Button variant="outline-secondary">Mon {Student.availTime[1]}</Button>
-                <Button variant="outline-secondary">Tue {Student.availTime[2]}</Button>
-                <Button variant="outline-secondary">Wed {Student.availTime[3]}</Button>
-                <Button variant="outline-secondary">Thu {Student.availTime[4]}</Button>
-                <Button variant="outline-secondary">Fri {Student.availTime[5]}</Button>
-                <Button variant="outline-secondary">Sat {Student.availTime[6]}</Button>
+                Log in to view your profile.
             </div>
-            <div>
-                <Button variant="outline-secondary">Sun {Student.availTime[7]}</Button>
-                <Button variant="outline-secondary">Mon {Student.availTime[8]}</Button>
-                <Button variant="outline-secondary">Tue {Student.availTime[9]}</Button>
-                <Button variant="outline-secondary">Wed {Student.availTime[10]}</Button>
-                <Button variant="outline-secondary">Thu {Student.availTime[11]}</Button>
-                <Button variant="outline-secondary">Fri {Student.availTime[12]}</Button>
-                <Button variant="outline-secondary">Sat {Student.availTime[13]}</Button>
-            </div>
-            <div>
-                <Button variant="outline-secondary">Sun {Student.availTime[14]}</Button>
-                <Button variant="outline-secondary">Mon {Student.availTime[15]}</Button>
-                <Button variant="outline-secondary">Tue {Student.availTime[16]}</Button>
-                <Button variant="outline-secondary">Wed {Student.availTime[17]}</Button>
-                <Button variant="outline-secondary">Thu {Student.availTime[18]}</Button>
-                <Button variant="outline-secondary">Fri {Student.availTime[19]}</Button>
-                <Button variant="outline-secondary">Sat {Student.availTime[20]}</Button>
-            </div>
-            <div>
-                <Button variant="outline-secondary">Sun {Student.availTime[21]}</Button>
-                <Button variant="outline-secondary">Mon {Student.availTime[22]}</Button>
-                <Button variant="outline-secondary">Tue {Student.availTime[23]}</Button>
-                <Button variant="outline-secondary">Wed {Student.availTime[24]}</Button>
-                <Button variant="outline-secondary">Thu {Student.availTime[25]}</Button>
-                <Button variant="outline-secondary">Fri {Student.availTime[26]}</Button>
-                <Button variant="outline-secondary">Sat {Student.availTime[27]}</Button>
-            </div>
-        </div>
-    );
+        );
+    }
 }
