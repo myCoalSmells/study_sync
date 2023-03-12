741611c9947f727763711dc009ab2167d6204e5b
Joshua Kim <jdkim5136@gmail.com>
2023-03-11 21:56:46 -0800

    Majority of Checkmarks are still in.
    Still fixing the issue where checkmarks are all in one day

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 // css modules
 import EPMod from "./EditProfile.module.css";
+import PPMod from "./ProfilePage.module.css";
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -58,17 +59,7 @@ export default function EditProfile() {
         }, 2300);
     };
 
-    // handle checkbox
-    // const handleCheck = (e) => {
-    //     const availTimeArr = Student.availTime.split('');
-    //     if (e.target.checked === true) {
-    //         availTimeArr[index] = '0';
-    //     } else {
-    //         availTimeArr[index] = '1';
-    //     }
-    //     const updatedAvailTimeArr = availTimeArr.join('');
-    //     setAvailTime(updatedAvailTimeArr);
-    // }
+    // checkbox check
     const handleCheck = (isChecked, index, e) => {
         const availTimeArr = availTime.split('');
         if (isChecked) {
@@ -78,6 +69,40 @@ export default function EditProfile() {
         }
         const updatedAvailTimeArr = availTimeArr.join('');
         setAvailTime(updatedAvailTimeArr);
+    };
+
+    // generate the schedule table
+    function generateTableRows() {
+        const times = ['12-1 AM', '1-2 AM', '2-3 AM', '3-4 AM', '4-5 AM', '5-6 AM', '6-7 AM', '7-8 AM', '8-9 AM', '9-10 AM', '10-11 AM', '11-12 AM', '12-1 PM', '1-2 PM', '2-3 PM', '3-4 PM', '4-5 PM', '5-6 PM', '6-7 PM', '7-8 PM', '8-9 PM', '9-10 PM', '10-11 PM', '11-12 PM'];
+        const tableRows = [];
+        
+        for (let i = 0; i < times.length; i++) {
+            const day = times[i];
+            const availTimes = [];
+        
+            // populate availability times for current day
+            for (let j = 0; j < 7; j++) {
+                const index = i + (j * 7);
+                availTimes.push(availTime[index]);
+            }
+        
+            // generate table row with availability times for current day
+            tableRows.push(
+                <tr key={day}>
+                    <td className={styles.text}>{day}</td>
+                    {availTimes.map((time, index) => (
+                        <input type="checkbox" checked={availTime[index] === '1'} onChange={(e) => handleCheck(e.target.checked, index)} />
+                    ))}
+                    <td></td>
+                </tr>
+            );
+        }
+    
+        return tableRows;
+    }
+    const styles = {
+        text: PPMod.text,
+        highlight: PPMod.highlight,
     };
 
     return(
@@ -106,10 +131,29 @@ export default function EditProfile() {
                 
                 <div className={EPMod.subcontainer}>
                     <label htmlFor="availTime">Available Times:</label>
-                    <input type="text" id="availTime" value={availTime} onChange={(e) => setAvailTime(e.target.value)} />
+                    <textarea value={availTime} readOnly='true' style={{fontSize: "16px"}}/>
 
                     <input type="checkbox" checked={availTime[0] === '1'} onChange={(e) => handleCheck(e.target.checked, 0)} />
 
+                </div>
+                <div className={PPMod.container}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th className={PPMod.text}>Time</th>
+                                <th className={PPMod.text}>Sun</th>
+                                <th className={PPMod.text}>Mon</th>
+                                <th className={PPMod.text}>Tue</th>
+                                <th className={PPMod.text}>Wed</th>
+                                <th className={PPMod.text}>Thu</th>
+                                <th className={PPMod.text}>Fri</th>
+                                <th className={PPMod.text}>Sat</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {generateTableRows()}
+                        </tbody>
+                    </table>
                 </div>
 
                 <div className={EPMod.subcontainer}>
