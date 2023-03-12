68fa4ed57ad3f8497153207f3296c2c9bbda8a78
Harry Hinman <harryhinman257@gmail.com>
2023-03-11 20:02:33 -0800

    logout also logs user out of firebase now

--- src/LogoutPage.js
+++ src/LogoutPage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LPMod from "./LoginPage.module.css";
 import { Link, useNavigate } from 'react-router-dom'
+import { auth } from "./firebase-setup/firebase";
+
 
 export default function LogoutPage() {
     const navigate = useNavigate();
+    useEffect(() => {
+        auth.signOut().then(function() {
+            console.log('Signed Out');
+        }, function(error) {
+            console.error('Sign Out Error', error);
+        });
+    });
     return (
         <div className={LPMod.login}>
         <h1>You have been logged out.</h1>
