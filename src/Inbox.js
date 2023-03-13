c2eb088335dc0c31a36907a6178461063dd6b67f
Harry Hinman <harryhinman257@gmail.com>
2023-03-13 01:35:18 -0700

    janky bit string implementation
    calculate schedule similarity between two matched people though janky fake bit comparison

--- src/Inbox.js
+++ src/Inbox.js
@@ -13,6 +13,7 @@ function Inbox() {
   const [matches, setMatches] = useState([])
   const [login, setLogin] = useState(false);
   const [matches_arr, setMatchesArr] = useState([]);
+  const [availTime, setAvailTime] = useState("");
   
 
   useEffect(() => {
@@ -22,6 +23,7 @@ function Inbox() {
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
           setMatches(docSnap.get("matches") || []);
+          setAvailTime(docSnap.get("availTime"));
           console.log("got matches");
           console.log(matches);
         } else {
@@ -50,11 +52,49 @@ function Inbox() {
         const _name = docSnap.get("username");
         const _classes = docSnap.get("classes");
         const _email = docSnap.get("email");
+        const _time = docSnap.get("availTime");
         let _pfp = docSnap.get("pfp");
         //const _profileLink = "/login/"+toString(id);
         if (!_pfp){
           _pfp = "https://i.pinimg.com/originals/1a/68/f7/1a68f758cd8b75d47e480722c3ad6791.png";
         }
+
+
+        // janky bit string comparison implementation
+        onAuthStateChanged(auth, async (user) => {
+          const docRef = doc(firestore, "students", user.uid);
+          const docSnap = await getDoc(docRef);
+          if (docSnap.exists()) {
+              setAvailTime(docSnap.get("availTime"));
+          }
+        });
+        let bitwiseAnd = "";
+        if (availTime.length > _time.length) {
+          for (let i = 0; i < _time.length; i++) {
+            if (availTime[i] === _time[i]) {
+              bitwiseAnd = bitwiseAnd + "1";
+            } else {
+              bitwiseAnd = bitwiseAnd + "0";
+            }
+          }
+        } else {
+          for (let i = 0; i < availTime.length; i++) {
+            if (availTime[i] === _time[i]) {
+              bitwiseAnd = bitwiseAnd + "1";
+            } else { 
+              bitwiseAnd = bitwiseAnd + "0";
+            }
+          }
+        }
+        let mergeTime = 0;
+        for (let i = 0; i < bitwiseAnd.length; i++) {
+          if (bitwiseAnd[i]) {
+            mergeTime = mergeTime + 1;
+          }
+        }
+        let _similarity = Math.trunc(mergeTime/168 * 100).toString() + "% Availability";
+
+
         let format = (
           <div className={InboxMod.box}>
             <div className={InboxMod["left-box"]} style={{ backgroundImage: `url(${_pfp})` }}></div>
@@ -62,6 +102,7 @@ function Inbox() {
               <div className={InboxMod.field}>{_classes.join(", ")}</div>
               <div className={InboxMod.field}>{_name}</div>
               <div className={InboxMod.field}>{_email}</div>
+              <div className={InboxMod.field}>{_similarity}</div>
             </div>
             <Link to={`/profile/${id}`}>
             <button>View Profile</button>
